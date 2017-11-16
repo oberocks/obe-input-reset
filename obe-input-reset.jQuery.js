7b4c8b4e77c0791bf0d2f1a5205e2d53940211bb
oberocks <matt@mattmct.com>
2017-11-16 17:59:53 -0500

    super clean

--- obe-input-reset.jQuery.js
+++ obe-input-reset.jQuery.js
@@ -9,21 +9,21 @@
 */
 
 (function ( $ ) {
- 
-    $.fn.obeInputReset = function( options ) {
- 
-        // Set Default Options
-    	// (and override with user options)
-        var settings = $.extend({
-			
+
+	$.fn.obeInputReset = function( options ) {
+
+		// Set Default Options
+		// (and override with user options)
+		var settings = $.extend({
+
 			// Default options
 			iconMode: false,  // Expects: true | false (Default = "false")
 			iconClasses: "fa fa-remove",  // Expects: string of icon classes (Default = "fa fa-remove")
 			text: "X"  // Expects: string (Default = "X")
-			
-        }, options );
-        
-        
+	
+		}, options );
+
+
 		// Construct Markup Elements
 		var wrapper = document.createElement("div");
 		// add a class name to the wrapper for JS/CSS functionality
@@ -31,40 +31,41 @@
 		var span = document.createElement("span");
 		// add a class name to the span for JS/CSS functionality
 		span.className = "obe-input-reset";
-        
-        
-        if (settings.iconMode === false) {
-        	
-        	var content = document.createTextNode(settings.text);
-        	
-        } else if (settings.iconMode === true)  {
-        	
-        	if (settings.iconClasses.length > 0) {
-        		
-        		var content = document.createElement("i");
-        		content.className = settings.iconClasses;
-        			
-        	}
-        		
-        }
-        
-        
+
+
+		if (settings.iconMode === false) {
+
+			var content = document.createTextNode(settings.text);
+
+		} else if (settings.iconMode === true)  {
+
+			if (settings.iconClasses.length > 0) {
+
+				var content = document.createElement("i");
+				content.className = settings.iconClasses;
+	
+			}
+	
+		}
+
+
+
 		// Create click listener for elements created by this script
 		$("body").on( "click", ".obe-input-reset", function() {
-			
+
 			$(this).parent().prev().val('').focus();
-			
+
 		});
-		
-		
+
+
 		// Nest Elements and prepare for output
 		span.appendChild(content);
 		wrapper.appendChild(span);
-		
-		
-        // Return output
-        return $(wrapper).insertAfter(this);
- 
-    };
- 
+
+
+		// Return output
+		return $(wrapper).insertAfter(this);
+
+	};
+
 }( jQuery ));
