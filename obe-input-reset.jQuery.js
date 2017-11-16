4ff4d5a03016dbaf292623ff575617989deb8943
oberocks <matt@mattmct.com>
2017-11-16 16:06:14 -0500

    added copyright and version info
    to both .css and .js files

--- obe-input-reset.jQuery.js
+++ obe-input-reset.jQuery.js
@@ -1,3 +1,13 @@
+/*!
+ * OBE Input Reset v1.0.0
+ * A lightweight and customizable jQuery plugin to add a clickable character to input elements
+ * with click functionality to clear the input value, and focus the input.
+ *
+ * Licensed GPLv3 for open source use
+ * Copyright (C) Matt McT Designs 2017
+ * 
+*/
+
 (function ( $ ) {
  
     $.fn.obeInputReset = function( options ) {
